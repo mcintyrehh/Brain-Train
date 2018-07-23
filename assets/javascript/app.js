d08ef3c823269cc415fe5ae9ec16d25d66e8f30c
Onray <mcintyrehh@gmail.com>
2018-07-22 21:32:37 -0400

    data pulls from firebase now!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,19 +10,50 @@ $(document).ready(function () {
   };
   firebase.initializeApp(config);
 
+  var keys = [];
   var dataRef = firebase.database().ref("trains");
-  dataRef.push({
-    trainName: "",
-    destination: "",
-    frequency: "",
-    nextArrival: "",
-    minutesAway: "",
-  });
-  dataRef.on("child_added", function (keySnapshot) {
-    var keys = [];
-    keys.push(keySnapshot);
-    console.log(keySnapshot);
+  var oakKey = "-LI2tjawUqlYWUnl-VTR";
+  var oakToOak;
+  console.log(oakToOak);
+
+
+  dataRef.on("child_added", function(snapshot) {
+    keys.push(snapshot.key);
+    console.log(snapshot.key);
     console.log(keys);
   });
+  dataRef.on('value', function(snapshot) {
+    console.log(snapshot.val());
+    oakToOak = snapshot.val()[oakKey];
+    console.log(oakToOak);
+    console.log(oakToOak.trainName);
+    appendTrain(oakToOak);
+  });
+
+  // appendTrain(oakToOak);
+  console.log("global " + oakToOak);
+  function appendTrain(train) {
+    var newRow = $('<tr>');
+    var newTD = $('<td>');
+    newRow.append('<td>' + train.trainName + '</td>');
+    newRow.append('<td>' + train.destination + '</td>');
+    newRow.append('<td>' + train.frequency + '</td>');
+    newRow.append('<td>' + "Next Arrival" + '</td>');
+    newRow.append('<td>' + "Mins Away" + '</td>');
+    $('.mainTable').append(newRow);
+  }
+
+
+
+  function addTrain() {
+    dataRef.push({
+      trainName: "",
+      destination: "",
+      frequency: "",
+      nextArrival: "",
+      minutesAway: "",
+
+    });
+  }
 });
 
