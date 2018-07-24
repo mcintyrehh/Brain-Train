0b36b4c43a99d09e5a2ca636328c5c64123e43dd
Onray <mcintyrehh@gmail.com>
2018-07-23 22:51:50 -0400

    trains now pull info from firebase and calculate next arrial time/minutes until next train

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,46 +14,91 @@ $(document).ready(function () {
   var dataRef = firebase.database().ref("trains");
   var oakKey = "-LI2tjawUqlYWUnl-VTR";
   var oakToOak;
-  console.log(oakToOak);
+  var mins;
+  var nextOne;
+
+  var datetime;
+  var date;
+
+  //live updates time in the header ever second
+  var update = function () {
+    date = moment(new Date());
+    datetime.html(date.format("dddd, Do MMMM YYYY, h:mm:ss a"));
+  };
+  $(document).ready(function () {
+    datetime = $('.train-time');
+    update();
+    setInterval(update, 1000);
+  });
 
 
-  dataRef.on("child_added", function(snapshot) {
+  // dataRef.on("child_added", function (snapshot) {
+  //   keys.push(snapshot.key);
+  //   console.log(snapshot.key);
+  //   console.log(keys);
+  // });
+  dataRef.on('child_added', function (snapshot) {
     keys.push(snapshot.key);
-    console.log(snapshot.key);
-    console.log(keys);
-  });
-  dataRef.on('value', function(snapshot) {
-    console.log(snapshot.val());
-    oakToOak = snapshot.val()[oakKey];
-    console.log(oakToOak);
-    console.log(oakToOak.trainName);
-    appendTrain(oakToOak);
+    oakToOak = snapshot.val();
+    appendTrain(snapshot.val());
   });
 
-  // appendTrain(oakToOak);
-  console.log("global " + oakToOak);
+
   function appendTrain(train) {
+    nextArrival(train.firstTrain, train.frequency);
+    // console.log(nextTrainTime);
+    console.log(mins);
     var newRow = $('<tr>');
-    var newTD = $('<td>');
+  
     newRow.append('<td>' + train.trainName + '</td>');
     newRow.append('<td>' + train.destination + '</td>');
     newRow.append('<td>' + train.frequency + '</td>');
-    newRow.append('<td>' + "Next Arrival" + '</td>');
-    newRow.append('<td>' + "Mins Away" + '</td>');
+    newRow.append('<td>' + nextOne + '</td>');
+    newRow.append('<td>' + mins + '</td>');
     $('.mainTable').append(newRow);
   }
 
-
+  function nextArrival(firstTrain, interval) {
+    var firstTrainToday = moment(new Date()).format("dddd, Do MMMM YYYY " + firstTrain);
+    console.log(firstTrainToday);
+    //changing firstTrain time to include ms
+    console.log(firstTrain);
+    //the current time as a variable
+    var now = moment(new Date());
+    //first train of the day as a variable
+    var firstGuy = moment(firstTrainToday, 'dddd, Do MMMM YYYY HH:mm');
+    console.log('first train came at ' + firstGuy.format('HH:mm'));
+    //how many minutes have passed since the first train
+    var diff = now.diff(firstGuy, 'minutes');   
+    console.log('mins since first train ' + diff);
+    //minutes passed/interval gives us the number of trains that have already gone by
+    var trainsPassed = Math.floor(diff/interval);
+    console.log('trains passed ' + trainsPassed);
+    //adding 1 to this gives us the next train's times
+    var nextTrain = (trainsPassed + 1) * interval;
+    console.log('next train time' + nextTrain);
+    //shows the time of the next train
+    var nextTrainTime = firstGuy.add(nextTrain, 'minutes').format('dddd, Do MMMM YYYY HH:mm');
+    var nextTrainHR = firstGuy.add(nextTrain, 'minutes').format('hh:mm a');
+    console.log(nextTrainHR);
+    var minutesAway = moment(nextTrainTime , 'Do MMMM YYYY HH:mm:ss').diff(now, 'minutes');
+    console.log('minutes away: ' + minutesAway);
+    mins = minutesAway;
+    nextOne = nextTrainHR;
+    console.log(nextOne);
+    console.log(mins);
+    // $('.test-time').html(nextTrainTime);
+  }
+  
 
   function addTrain() {
     dataRef.push({
-      trainName: "",
-      destination: "",
-      frequency: "",
-      nextArrival: "",
-      minutesAway: "",
-
+      destination: "Charlotte",
+      firstTrain: "04:00",
+      frequency: "120",
+      trainName: "QC: Queens Carriage"      
     });
   }
+  // addTrain();
 });
 
